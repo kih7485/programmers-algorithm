4606fd41471ec0131fd5bd06c22379e196d48f6e
inhan <korea4127@gmail.com>
2022-07-21 01:05:40 +0900

    봉우리

--- test.js
+++ test.js
@@ -1,20 +1,11 @@
-function solution(subway, start, end) {
-    var answer = 0;
-    //console.log(subway);
-    for (let i = 0; i < subway.length; i++){
-        const subwayLines = subway[i].split(" ");
-        const subwayStartLine = subway[i].indexOf(start);
-        for (let j = 0; j < subway.length; j++) {
-        
-        }
-        //console.log(i);
-        //const subwayLines = subway[i];
-        
-        const subwayEndLine = subway[i].indexOf(end);
-        console.log(subwayEndLine);
-    }
-    return answer;
+function solution(n, a, b) {
+  let answer = 0;
+  while (a !== b) {
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    answer++;
+  }
+  return answer;
 }
 
-
-solution(["1 2 3 4 5 6 7 8", "2 11", "0 11 10", "3 17 19 12 13 9 14 15 10", "20 2 21"], 1, 9);
+solution(8, 4, 7);
