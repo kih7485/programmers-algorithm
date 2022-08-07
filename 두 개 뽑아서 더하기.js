8e1c63f0d664a1ac2fc95ab6f429fe2271367247
inhan <korea4127@gmail.com>
2022-08-07 16:50:03 +0900

    시저암호

--- 두 개 뽑아서 더하기.js
+++ 두 개 뽑아서 더하기.js
@@ -11,13 +11,15 @@
 // [5,0,2,7]	[2,5,7,9,12]
 
 function solution(numbers) {
-    let result = [];
-    for(let i = 0; i < numbers.length; i++){
-        for(let j = i+1; j < numbers.length; j++){
-            if(!(result.includes(numbers[i] + numbers[j]))){
-                result.push(numbers[i] + numbers[j])
-            }            
-        }
+  let result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (!result.includes(numbers[i] + numbers[j])) {
+        result.push(numbers[i] + numbers[j]);
+      }
     }
-    return result.sort((a, b) => a - b);
+  }
+  return result.sort((a, b) => a - b);
 }
+
+console.log(solution([2, 1, 3, 4, 1]));
